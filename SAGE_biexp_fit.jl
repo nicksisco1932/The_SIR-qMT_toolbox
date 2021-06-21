a7ffd5ee3057669c5109171b98fd27a4db7364e9
nicksisco1932 <nicksisco1932@gmail.com>
2021-06-21 12:54:10 -0700

    Update SAGE_biexp_fit.jl

--- SAGE_biexp_fit.jl
+++ SAGE_biexp_fit.jl
@@ -82,90 +82,93 @@ end
 
 a = commandline()
 
-base = dirname(a["TE_1"])
-paths = [joinpath(base,basename(a["TE_1"])),
-    joinpath(base,basename(a["TE_2"])),
-    joinpath(base,basename(a["TE_3"])),
-    joinpath(base,basename(a["TE_4"])),
-    joinpath(base,basename(a["TE_5"]))
-    ]
-b_fname = joinpath(base,basename(a["SAGE_brainMask"]))
+function main()
+    base = dirname(a["TE_1"])
+    paths = [joinpath(base,basename(a["TE_1"])),
+        joinpath(base,basename(a["TE_2"])),
+        joinpath(base,basename(a["TE_3"])),
+        joinpath(base,basename(a["TE_4"])),
+        joinpath(base,basename(a["TE_5"]))
+        ]
+    b_fname = joinpath(base,basename(a["SAGE_brainMask"]))
 
-# base = parsed_args["input"]
+    # base = parsed_args["input"]
 
-# paths = [ @sprintf("%sPT1319001_TE1_img_w_Skull.nii.gz",base),
-#             @sprintf("%sPT1319001_TE2_img_w_Skull.nii.gz",base),
-#             @sprintf("%sPT1319001_TE3_img_w_Skull.nii.gz",base), 
-#             @sprintf("%sPT1319001_TE4_img_w_Skull.nii.gz",base),
-#             @sprintf("%sPT1319001_TE5_img_w_Skull.nii.gz",base)
-#             ]
+    # paths = [ @sprintf("%sPT1319001_TE1_img_w_Skull.nii.gz",base),
+    #             @sprintf("%sPT1319001_TE2_img_w_Skull.nii.gz",base),
+    #             @sprintf("%sPT1319001_TE3_img_w_Skull.nii.gz",base), 
+    #             @sprintf("%sPT1319001_TE4_img_w_Skull.nii.gz",base),
+    #             @sprintf("%sPT1319001_TE5_img_w_Skull.nii.gz",base)
+    #             ]
 
-# # paths = [ @sprintf("%sMFA_10_angles.nii.gz",base)]
-# b_fname = @sprintf("%sbPT1319001_preb_mask.nii.gz",base)
+    # # paths = [ @sprintf("%sMFA_10_angles.nii.gz",base)]
+    # b_fname = @sprintf("%sbPT1319001_preb_mask.nii.gz",base)
 
-DATA,MASK,nx,ny,nz,ne,nt=load_sage_data(paths,b_fname);
-tot_voxels = nx*ny*nz
-vec_mask = reshape(MASK[:,:,:,1,1],tot_voxels,1);
+    DATA,MASK,nx,ny,nz,ne,nt=load_sage_data(paths,b_fname);
+    tot_voxels = nx*ny*nz
+    vec_mask = reshape(MASK[:,:,:,1,1],tot_voxels,1);
 
-vec_data = reshape(DATA,tot_voxels,ne,nt)
+    vec_data = reshape(DATA,tot_voxels,ne,nt)
 
 
-thr = Threads.nthreads()
-if thr==16
-    Threads.nthreads() = 8
-    return thr = 8
-end
-println("Fitting with $thr threads")
-
-te1=0.007653;
-te2=0.027969;
-te3=0.059074;
-te4=0.07939;
-te5=0.099706;
-echos=[te1,te2,te3,te4,te5]
-TR = 1800/1000;
-
-X0=[1,100.0,50,1]
-IND=findall(x->x.>0,vec_mask[:,1]);
-
-function optim_fitty(f::Function, xdata::Vector{Float64},data::Vector{Float64},x0::Vector{Float64})
-    # fitdata = Optim.minimizer( optimize(b -> sqerrorSAGE(b, xdata,  data), x0))
-    fitdata = curve_fit(f,xdata,data,x0;autodiff=:finiteforward) # this is faster and has good results
-    return fitdata
-end
-
-fitdata = zeros(tot_voxels,4,nt);
-@time for JJ=1:150;
-    for ii in IND
-        temp = vec_data[ii,:,JJ];
-        fitY = optim_fitty(SAGE_biexp3p_d, echos, temp, X0)
-        # fitY = optim_fitty(echos, temp, X0)
-        fitdata[ii,:,JJ] = fitY.param
+    thr = Threads.nthreads()
+    if thr==16
+        Threads.nthreads() = 8
+        return thr = 8
     end
+    println("Fitting with $thr threads")
+
+    te1=0.007653;
+    te2=0.027969;
+    te3=0.059074;
+    te4=0.07939;
+    te5=0.099706;
+    echos=[te1,te2,te3,te4,te5]
+    TR = 1800/1000;
+
+    X0=[1,100.0,50,1]
+    IND=findall(x->x.>0,vec_mask[:,1]);
+
+    function optim_fitty(f::Function, xdata::Vector{Float64},data::Vector{Float64},x0::Vector{Float64})
+        # fitdata = Optim.minimizer( optimize(b -> sqerrorSAGE(b, xdata,  data), x0))
+        fitdata = curve_fit(f,xdata,data,x0;autodiff=:finiteforward) # this is faster and has good results
+        return fitdata
+    end
+
+    fitdata = zeros(tot_voxels,4,nt);
+    @time for JJ=1:150;
+        for ii in IND
+            temp = vec_data[ii,:,JJ];
+            fitY = optim_fitty(SAGE_biexp3p_d, echos, temp, X0)
+            # fitY = optim_fitty(echos, temp, X0)
+            fitdata[ii,:,JJ] = fitY.param
+        end
+    end
+    #-------------------------------------  
+    Xv = Array{Float64}(fitdata);
+    Xv = reshape(Xv,nx,ny,nz,4,nt);
+    R2s = zeros(nx,ny,nz,nt);
+    R2 = zeros(nx,ny,nz,nt);
+    R2s = Xv[:,:,:,2,:];
+    R2 = Xv[:,:,:,3,:];
+    R2s[findall(x->x.>1000,R2s)].=0
+    R2s[findall(x->x.<0,R2s)].=0
+    R2[findall(x->x.>1000,R2)].=0
+    R2[findall(x->x.<0,R2)].=0
+    #-------------------------------------
+
+    d=niread(paths[1]);
+
+    tmp1 = voxel_size(d.header)[1]
+    tmp2 = voxel_size(d.header)[2]
+    tmp3 = voxel_size(d.header)[3]
+
+    R2s_fname = @sprintf("%sSAGE_R2s_Brain_julia.nii.gz",base)
+    R2_fname = @sprintf("%sSAGE_R2_Brain_julia.nii.gz",base)
+    niwrite(R2s_fname,NIVolume(R2s;voxel_size=(tmp1,tmp2,tmp3)))
+    niwrite(R2_fname,NIVolume(R2;voxel_size=(tmp1,tmp2,tmp3)))
+
+    println("The R2s is saved at $R2s_fname")
+    println("The R2 is saved at $R2_fname")
 end
-#-------------------------------------  
-Xv = Array{Float64}(fitdata);
-Xv = reshape(Xv,nx,ny,nz,4,nt);
-R2s = zeros(nx,ny,nz,nt);
-R2 = zeros(nx,ny,nz,nt);
-R2s = Xv[:,:,:,2,:];
-R2 = Xv[:,:,:,3,:];
-R2s[findall(x->x.>1000,R2s)].=0
-R2s[findall(x->x.<0,R2s)].=0
-R2[findall(x->x.>1000,R2)].=0
-R2[findall(x->x.<0,R2)].=0
-#-------------------------------------
-
-d=niread(paths[1]);
-
-tmp1 = voxel_size(d.header)[1]
-tmp2 = voxel_size(d.header)[2]
-tmp3 = voxel_size(d.header)[3]
-
-R2s_fname = @sprintf("%sSAGE_R2s_Brain_julia.nii.gz",base)
-R2_fname = @sprintf("%sSAGE_R2_Brain_julia.nii.gz",base)
-niwrite(R2s_fname,NIVolume(R2s;voxel_size=(tmp1,tmp2,tmp3)))
-niwrite(R2_fname,NIVolume(R2;voxel_size=(tmp1,tmp2,tmp3)))
-
-println("The R2s is saved at $R2s_fname")
-println("The R2 is saved at $R2_fname")
+main()
