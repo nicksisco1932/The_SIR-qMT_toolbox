65a06a7d7f3de0e9931a27586dade06207bcb03a
nicksisco1932 <nicksisco1932@gmail.com>
2021-06-22 15:37:25 -0700

    Update denoiseMatrix.jl

--- denoiseMatrix.jl
+++ denoiseMatrix.jl
@@ -2,6 +2,11 @@
 using LinearAlgebra
 using Statistics
 function  denoiseMatrix(X) 
+
+    #= 
+    From Mark Does Implimentation 
+    =#
+
     M,N = size(X);
     minMN = minimum([M N]);
     Xm = mean(X,dims=2)
@@ -24,35 +29,6 @@ function  denoiseMatrix(X)
         U,S,V = F;
         lambda = S.^2/N;
     end
-    #= From MP-denoise, oddly written hard to covert
-    # order_orig = sortperm(lambda);
-    # lambda = sort(lambda, rev=true);
-    # order = sortperm(lambda, rev=true);
-    # U = U[:,order];
-    # csum = cumsum(lambda[order_orig], dims = 2);
-    # # p = Array{Float64}([0:length(lambda)-1]');
-    # p = collect(0:length(lambda)-1);
-    
-    # A = (lambda.-lambda[end]).*(M.-p).*(N.-p)
-    # B = 4*csum*sqrt(M*N)
-    # p = -1 .+ p[(A.<B)]
-
-    # if p==0
-    #     X = zeros(size(X));
-    # elseif M<N
-    #     Binv = Array{Float64}(U[:,1:minimum(p)]');
-    #     X = U[:,1:minimum(p)]*Binv.*X;
-    # else
-    #     # Binv = U[:,1:minimum(p)]';
-    #     Binv = Array{Float64}(U[:,1:minimum(p)]');
-    #     X[1:minimum(p),1:minimum(p)] = X.*U[:,1:minimum(p)].*Binv;
-    # end
-    # s2 = csum[p+1]/((M-p)*(N-p));
-    # s2_after = s2 - csum[p+1]/(M*N);
-    # return X,s2,p,s2_after 
-    =#
-    #= From Mark Does =#
-
     
     p = 0;
     pTest = false;
@@ -63,7 +39,7 @@ function  denoiseMatrix(X)
 
     while pTest == false
         sigma2 = (lambda[p+1] - lambda[minMN]) / (4*sqrt((M-p)/N))
-        pTest = sum(lambda[p+1:minMN]/scaling[p+1]) >= minMN-p*sigma2
+        pTest = sum(lambda[p+1:minMN]/scaling[p+1]) >= (minMN-p)*sigma2
         if pTest == false
             p += 1
         end
@@ -72,6 +48,8 @@ function  denoiseMatrix(X)
 
     invV = Array{Float64}(V[:,1:p]')
     newX = U[:,1:p] * S[1:p,1:p] * invV + Xm
+
+    return newX, sigma2, p
 
 end
 
@@ -95,18 +73,19 @@ echos=[te1,te2,te3,te4,te5]
 
 a = LinRange(75,150,128);
 b = LinRange(20,70,128);
-mat_img = zeros(128,128,5);
-R2_map = zeros(128,128);
-R2s_map = zeros(128,128);
-SNR=150;
-for ii in 1:128
-    for jj in 1:128
-        X0 = [1,a[ii],b[jj],500];
-        Yy = SAGE_biexp3p_d(echos,X0);
-        R2s_map[ii,jj] = a[ii]
-        R2_map[ii,jj] = b[ii]
-        mat_img[ii,jj,:] = Yy+rand(5)/SNR
-    end
-end
+# mat_img = zeros(128,128,15,5);
+# R2_map = zeros(128,128);
+# R2s_map = zeros(128,128);
+# SNR=150;
+# for ii in 1:128
+#     for jj in 1:128
+#         for kk in 1:15
+#             X0 = [1,a[ii],b[jj],500];
+#             Yy = SAGE_biexp3p_d(echos,X0);
+#             mat_img[ii,jj,kk,:] = Yy+rand(5)/SNR
+#         end
+#     end
+# end
+X = rand(128,5)
 
-denoiseMatrix(mat_img[:,:,1])
+denoiseMatrix(X)
