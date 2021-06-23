3c6c15a61acf7876881040d941de67fdf92d41d4
nicksisco1932 <nicksisco1932@gmail.com>
2021-06-22 17:48:41 -0700

    Update denoiseMatrix.jl

--- denoiseMatrix.jl
+++ denoiseMatrix.jl
@@ -38,7 +38,61 @@ function  denoiseMatrix(X)
 
 end
 
+function denoiseVol(vol,w,mask=[])
+    nx_orig,ny_orig,nz_orig,nt_orig = size(vol);
+    nx,ny,nz,nt = size(vol);
+
+    if mask == Any[]
+        mask = ones(size(vol[:,:,:,1]));
+    end
+
+    @assert(length(w)>1 && length(w)<4)
+
+    if length(w) == 2
+        tmp= ones(size(x)[1]+1)
+        tmp[1:2] = w
+        w = tmp
+    end
+
+    M = nt;
+    N = prod(w)
+    denoised = zeros(size(vol));
+
+    p = zeros(nx,ny,nz);
+    S2 = zeros(nx,ny,nz);
+
+    m = nx - w[1]+1;
+    n = ny - w[2]+1;
+    o = nz - w[3]+1;
+    
+
+    for (count,ii) in enumerate(1:m*n*o)
+        
+        k =  floor((1-1)/m/n)+1;
+        j = floor((ii-1-(k-1)*m*n)/m )+1
+        i = ii - (k-1)*m*n-(j-1)*m;
+
+        rows = Array{Int64}(collect(i:i-1+w[1]))
+        cols = Array{Int64}(collect(j:j-1+w[2]))
+        slis = Array{Int64}(collect(k:k-1+w[3]))
+
+        maskCheck = reshape(mask[rows,cols,slis],N,1)
+
+        X = reshape(vol[rows,cols,slis,:],M,N)
+
+    end
+
+    
+end
 
 X = rand(128,5)
 
 dnX,sig,p = denoiseMatrix(X)
+
+w = 5
+
+using RollingFunctions
+
+X = rand(128,128,15,5);
+
+rolling(denoiseMatrix,X,w)
