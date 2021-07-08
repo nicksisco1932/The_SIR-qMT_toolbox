6e48859fed8a1cf1b1b47fa82946c6553d75e8bb
nicksisco1932 <nicksisco1932@gmail.com>
2021-07-07 17:08:09 -0700

    Update CBF_calc_volterra.m

--- Matlab/SAGE_DSC/mfiles/CBF_calc_volterra.m
+++ Matlab/SAGE_DSC/mfiles/CBF_calc_volterra.m
@@ -1,36 +1,87 @@
-function [CBF_map,CBFSE_map] = CBF_calc(dtemp_all,dtemp_allSE,DSC,U,S,V,maxS,S_orig,brain_img,threshold)
-    % % %Calculate CBF*R(t)
-    CBF_map = zeros(size(dtemp_all));
-    CBFSE_map = zeros(size(dtemp_allSE));
+function [CBF_map,CBFSE_map] = CBF_calc_volterra(DSC,brain_img,threshold)
+        
+    %--------------------
+    DeltaT = DSC.Parms.TR; % sampling interval
+    
+    options.nT=DSC.Parms.nt;
+    disp('Doing Volterra Method.')
+    nt = DSC.Parms.nt;
+    nTpad=2*nt;
+    columnG=zeros(nTpad,1);
+    AIF=DSC.AIF(2,:)';
+    a = zeros(nTpad,1);
+    a(1:nt) = AIF; % For continuity with literature and formal matrix notation
+    G=zeros(nTpad);
+    for ii = 2:nt-1
+        for jj = ii:nt-1
+            t=ii;
+            G(jj,ii) = (2*a(t)*DeltaT/6)+(2*a(t)*DeltaT/6); % lower triangle matrix
+        end
+    end
+    for ii = 2:nt-1
+        jj = ii;
+        n=ii-1;
+        G(ii,jj) = diag(2*a(n)-a(n)*DeltaT/6); % a-  diag (aii)
+    end
+    G(1:nt-1,1) = 2*a(1:nt-1)-a(1:nt-1)*DeltaT/6; %a- column 1 (ai0)
+    
+    [U,S,V]=svd(G);
+    maxS = max(diag(S));
+    S_orig = S;
+    
+    %%%Calculate CBF*R(t)
+    % CBFR_all = zeros(size(dtemp_all)); % don't need these, the max is taken
+    % in the loop to eliminate a step
+    % CBFR_allSE = zeros(size(dtemp_allSE));
+    CBF_map=zeros(DSC.Parms.nx,DSC.Parms.ny,DSC.Parms.nz);
+    CBFSE_map=zeros(DSC.Parms.nx,DSC.Parms.ny,DSC.Parms.nz);
+    nTpad=size(G,1); % renamed here
+    residual_cbf=zeros(nx,ny,nz,nTpad);
+    residual_cbfse=zeros(nx,ny,nz,nTpad);
+    mask=brain_img; % Adding the brain index here is much much faster
     for i = 1:DSC.Parms.nx
         for j = 1:DSC.Parms.ny
             for k = 1:DSC.Parms.nz
-                if brain_img(i,j,k) % much faster with this
+                if mask(i,j,k) % don't do anything outside the mask
+                    % Apply adaptive threshold
                     S = S_orig;
-                    S(S<(threshold(i,j,k)*maxS))=0;
-                    %Inverts the diagonal of the S matrix produced through SVD
-                    S = 1./S;
-                    S(isinf(S)) = 0;
+                    adapt_thresh = threshold(i,j,k)*maxS;
                     %Computes the inverted DeltaR2star values for the AIF
-                    invDeltaR2starAIF=V*S*U';
-                    CBF_map(i,j,k,:) = invDeltaR2starAIF*squeeze(dtemp_all(i,j,k,:));
-                    CBFSE_map(i,j,k,:) = invDeltaR2starAIF*squeeze(dtemp_allSE(i,j,k,:));
+                    eigenV=diag(S);
+                    eigenV(eigenV<adapt_thresh)=0;
+                    %                 newEigen=eigenV;
+                    newEigen=eigenV.^-1;
+                    newEigen(isinf(newEigen))=0;
+                    Ginv=V*diag(newEigen)*(U'); % the second diagonal puts the eiganvector back into a mxn matrix
+                    vettConc=zeros(nTpad,1);
+                    vettConc(1:nt)=reshape(CTC_all(i,j,k,:),nt,1);
+                    vettRes=(1/TR)*Ginv*vettConc;
+                    CBF_map(i,j,k)=max(abs(vettRes));
+                    residual_cbf(i,j,k,:)=vettRes;
+                    vettConc=zeros(nTpad,1);
+                    vettConc(1:nt)=reshape(CTC_SE(i,j,k,:),nt,1);
+                    vettRes=(1/TR)*Ginv*vettConc;
+                    CBFSE_map(i,j,k)=max(abs(vettRes));
+                    residual_cbfse(i,j,k,:)=vettRes;
                 end
             end
         end
     end
-
-    ind=find(brain_img);
-    CBF_map(~ind)=0;
-    CBFSE_map(~ind)=0;
+    
+    CBF_map(CBF_map>1000)=0;
+    CBFSE_map(CBFSE_map>1000)=0;
+    
     CBF_map(isnan(CBF_map))=0;
-    CBFSE_map(isnan(CBFSE_map))=0;
     CBF_map(isinf(CBF_map))=0;
+    CBFSE_map(~ind)=0;
+    CBFSE_map(isnan(CBFSE_map))=0;
     CBFSE_map(isinf(CBFSE_map))=0;
-
-
-    CBF_map = max(CBF_map,[],4);
-    CBF_map(~isfinite(CBF_map)) = 0;
-    CBFSE_map = max(CBFSE_map,[],4);
-    CBFSE_map(~isfinite(CBFSE_map)) = 0;
+    
+    %         CBF_map = removeOutlierVolume(CBF_map,3,[3 3 3]);
+    %         CBFSE_map = removeOutlierVolume(CBFSE_map,3,[3 3 3]);
+    
+    CBF_map(CBF_map>100)=0;
+    CBFSE_map(CBFSE_map>100)=0;
+    
+    
 end
