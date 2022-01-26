0c64c01308c7c660838206f00fab6c28c9110c1b
nicksisco1932 <nicksisco1932@gmail.com>
2022-01-25 17:23:02 -0700

    Update SIR_fit.jl
        "    - Got rid of main function and ran everything on high level.
        "    - Added a for loop funtion and tested with @btime macro.
        "    - TODO: Please delete the @btime macro when needed.

--- SIR_fit.jl
+++ SIR_fit.jl
@@ -45,20 +45,26 @@
     "   - Major updated I/O 
         - Change fitting function and for loop 
         - Dramatic improvement in speed 
-        \n"
+    \n"
     "1.1  Sept 8, 2021 [nsisco]\n"
     "     (Nicholas J. Sisco, Ph.D. of Barrow Neurological Institue)\n"
     "    - Another dramatic improvement in speed 
     "    - Removed some conditionals and made type-stable
-        \n"    
+    \n"    
     "1.2  Sept 16, 2021 [nsisco]\n"
     "     (Nicholas J. Sisco, Ph.D. of Barrow Neurological Institue)\n"
     "    - forwarddiff working 
-        \n"    
+    \n"    
     "1.2.1  Jan 25, 2022 [nsisco]\n"
     "     (Nicholas J. Sisco, Ph.D. of Barrow Neurological Institue)\n"
     "    - After review of manuscript, added suggestions from Tobias Wood.
-        \n"
+    \n"
+    "1.3  Jan 25, 2022 [nsisco]\n"
+    "     (Nicholas J. Sisco, Ph.D. of Barrow Neurological Institue)\n"
+    "    - Got rid of main function and ran everything on high level. 
+    "    - Added a for loop funtion and tested with @btime macro.
+    "    - TODO: Please delete the @btime macro when needed.
+    \n"
 
 =#
 using Pkg	
@@ -144,162 +150,175 @@ paths = [joinpath(base,"SIR_mo_corr.nii.gz")]
 b_fname = joinpath(base,"brain_mask.nii.gz")
 =#
 
-
-function main(a)
-    
-	# Set up paths base on user input
-    base = dirname(a["SIR_Data"])
-    paths = a["SIR_Data"]
-    b_fname = joinpath(base,basename(a["SIR_brainMask"]))
- 
-    #=----------------------------------------------
-    Variable Definitions
-    ----------------------------------------------
-    =#
-	# Previously, the fits would fail or generate nonsensical maps because the time units were wrong.
-	# This is now fixed with this if statement.
-    # If Error in TD unit handling. If the user puts in ms, convert to s
-    if a["TI"][1]>1
-        ti_times = a["TI"]* 1E-3
-        td_times = a["TD"]* 1E-3
-    else
-        ti_times = a["TI"]
-        td_times = a["TD"]
-    end
-	
-	# Set the kmf and Sm values here. 
-	# Future directions will change this to a fit parameter.
-    kmfmat = a["kmf"][1]
-    Smmat=a["Sm"][1]
-    X0 = [0.07, 1, -1, 1.5];
-
-    #=----------------------------------------------
-    Variable Definitions END
-    ----------------------------------------------
-    =#
-
-    
-    #=----------------------------------------------
-    Setting up data to be fit
-    ----------------------------------------------
-    =#
-    nx,ny,nz,nt = size(niread(paths))
-    temp=niread(paths)
-    temp2=Array{Float64}(temp.raw)
-    tempdata=Array{Float64}(zeros(nt,nx,ny,nz))
-	# switched the last column to the first b/c it is faster to loop through this way.
-    for ii in 1:nt
-        tempdata[ii,:,:,:] = temp2[:,:,:,ii];
-    end
-    DATA=tempdata;
-    MASKtmp = niread(b_fname);
-    MASK=Array{Bool}(MASKtmp.raw);
-
-    tot_voxels = nx*ny*nz
-    times=hcat(ti_times,td_times)
-    tmp = reshape(DATA,(nt,tot_voxels));
-    Yy = similar(tmp)
-    @simd for ii in 1:tot_voxels
-        @inbounds Yy[:,ii] = tmp[:,ii] / (tmp[end, ii])
-    end
-    
-    Yy[findall(x->isinf(x),Yy)].=0
-    Yy[findall(x->isnan(x),Yy)].=0
-    
-	# deleted this function b/c it made things overly complicated and did not add any value.
-    # tot_voxels,times,Yy = reshape_and_normalize( DATA,ti_times,td_times,nx,ny,nz,nt);
-    
-	# set up loop indeces to only loop over data within the mask
-    vec_mask = reshape(MASK,(tot_voxels));
-    ind = findall(x->x==Bool(1),vec_mask)
-
-    #=----------------------------------------------
-    Setting up data to be fit END
-    ----------------------------------------------
-    =#
-
-    #============ Fitting   ========#
-    
-    model(x,p) = SIR_Mz0(x,p,kmfmat,Sm=Smmat,R1m=NaN,mag=true)
-
-    ydata = zeros(4,length(ind))
-    for ii ∈ 1:length(ind)
-        ydata[:,ii] = Yy[:,ind[ii]]
+#TESTING: Making the fitting for loop a function
+function loop_fit(N::Integer,OUTMAT::Array{T},f::Function, xvals::Array{T},yvals::Array{T},guesses::Vector{T}) where T
+    # t = @elapsed for ii ∈ 1:l 
+    t = @elapsed Threads.@threads for ii ∈ 1:N # evaluated with @btime. Actually faster now.
+        @inbounds OUTMAT[ii,:] = nlsfit(f,xvals,yvals[:,ii],guesses)
     end
 
-   
-    l = length(ind)
-    tmpOUT = similar(Yy,l,4)
-    
-    t = @elapsed for ii ∈ 1:l # multi threading is actually slower
-    # t = @elapsed Threads.@threads for ii ∈ 1:l # multi threading is actually slower
-        # @inbounds tmpOUT[ii,:] = nlsfit(model,X,Yy[ii,:],X0)
-        @inbounds tmpOUT[ii,:] = nlsfit(model,times,ydata[:,ii],X0)
-    end 
-    println("The actual fit took $t seconds")
-    perS=Int(floor(l/t))
-    println("$perS voxels per second")
-    Xv = tmpOUT
-
-    _,n = size(Yy)    
-    fit_data = zeros(n,4);
-
-    for ii ∈ 1:length(ind)
-        fit_data[ind[ii],:] = Xv[ii,:]
-    end
-
-	# fill in preallocated maps, reshape, clip, and save the maps.
-    PSR = reshape(fit_data[:,1].*100,nx,ny,nz)
-    R1f = reshape(fit_data[:,2],nx,ny,nz)
-    Sf = reshape(fit_data[:,3],nx,ny,nz)
-
-    PSR[findall(x->x.>100,PSR)].=0
-    PSR[findall(x->x.<0,PSR)].=0
-    R1f[findall(x->x.>50,R1f)].=0
-    R1f[findall(x->x.<0,R1f)].=0
-
-    Sf[findall(x->x.>10,Sf)].=0
-    Sf[findall(x->x.<-10,Sf)].=0
-
-    d=niread(paths);
-
-    tmp1 = voxel_size(d.header)[1]
-    tmp2 = voxel_size(d.header)[2]
-    tmp3 = voxel_size(d.header)[3]
-
-    PSR_fname = @sprintf("%s/SIR_Brain_PSR_julia.nii.gz",base)
-    R1f_fname = @sprintf("%s/SIR_Brain_R1f_julia.nii.gz",base)
-    SF_fname = @sprintf("%s/SIR_Brain_Sf_julia.nii.gz",base)
-
-    fname_1 = @sprintf("%s/SIR_DATA.nii.gz",base)
-    niwrite(PSR_fname,NIVolume(PSR;voxel_size=(tmp1,tmp2,tmp3)))
-    niwrite(R1f_fname,NIVolume(R1f;voxel_size=(tmp1,tmp2,tmp3)))
-    niwrite(SF_fname,NIVolume(Sf;voxel_size=(tmp1,tmp2,tmp3)))
-
-    niwrite(fname_1,NIVolume(temp2;voxel_size=(tmp1,tmp2,tmp3)))
-
-    println(" ")
-    println(" ")
-    println(" ")
-    println("The PSR is saved at $PSR_fname")
-    println("The R1f is saved at $R1f_fname")
-    println("The Sf is saved at $SF_fname")
-    println("The original data was copied to $fname_1")
-    println(" in the same image space as the PSR map")
-    
+    return t,OUTMAT
 end
 
 println(" ")
 println(" ")
 println(" ")
-A = commandline()
+a = commandline()
 println(" ")
 println(" ")
 println(" ")
 
-tt = @elapsed main(A)
 
+    
+# Set up paths base on user input
+base = dirname(a["SIR_Data"])
+paths = a["SIR_Data"]
+b_fname = joinpath(base,basename(a["SIR_brainMask"]))
+
+#=----------------------------------------------
+Variable Definitions
+----------------------------------------------
+=#
+# Previously, the fits would fail or generate nonsensical maps because the time units were wrong.
+# This is now fixed with this if statement.
+# If Error in TD unit handling. If the user puts in ms, convert to s
+if a["TI"][1]>1
+    ti_times = a["TI"]* 1E-3
+    td_times = a["TD"]* 1E-3
+else
+    ti_times = a["TI"]
+    td_times = a["TD"]
+end
+
+# Set the kmf and Sm values here. 
+# Future directions will change this to a fit parameter.
+kmfmat = a["kmf"][1]
+Smmat=a["Sm"][1]
+X0 = [0.07, 1, -1, 1.5];
+
+#=----------------------------------------------
+Variable Definitions END
+----------------------------------------------
+=#
+
+
+#=----------------------------------------------
+Setting up data to be fit
+----------------------------------------------
+=#
+nx,ny,nz,nt = size(niread(paths))
+temp=niread(paths)
+temp2=Array{Float64}(temp.raw)
+tempdata=Array{Float64}(zeros(nt,nx,ny,nz))
+# switched the last column to the first b/c it is faster to loop through this way.
+for ii in 1:nt
+    tempdata[ii,:,:,:] = temp2[:,:,:,ii];
+end
+DATA=tempdata;
+MASKtmp = niread(b_fname);
+MASK=Array{Bool}(MASKtmp.raw);
+
+tot_voxels = nx*ny*nz
+times=hcat(ti_times,td_times)
+tmp = reshape(DATA,(nt,tot_voxels));
+Yy = similar(tmp)
+@simd for ii in 1:tot_voxels
+    @inbounds Yy[:,ii] = tmp[:,ii] / (tmp[end, ii])
+end
+
+Yy[findall(x->isinf(x),Yy)].=0
+Yy[findall(x->isnan(x),Yy)].=0
+
+# deleted this function b/c it made things overly complicated and did not add any value.
+# tot_voxels,times,Yy = reshape_and_normalize( DATA,ti_times,td_times,nx,ny,nz,nt);
+
+# set up loop indeces to only loop over data within the mask
+vec_mask = reshape(MASK,(tot_voxels));
+ind = findall(x->x==Bool(1),vec_mask)
+
+#=----------------------------------------------
+Setting up data to be fit END
+----------------------------------------------
+=#
+
+#============ Fitting   ========#
+
+model(x,p) = SIR_Mz0(x,p,kmfmat,Sm=Smmat,R1m=NaN,mag=true)
+
+ydata = zeros(4,length(ind))
+for ii ∈ 1:length(ind)
+    ydata[:,ii] = Yy[:,ind[ii]]
+end
+
+
+l = length(ind)
+tmpOUT = similar(Yy,l,4)
+
+# t = @elapsed for ii ∈ 1:l # multi threading is actually slower
+# # t = @elapsed Threads.@threads for ii ∈ 1:l # multi threading is actually slower
+#     # @inbounds tmpOUT[ii,:] = nlsfit(model,X,Yy[ii,:],X0)
+#     @inbounds tmpOUT[ii,:] = nlsfit(model,times,ydata[:,ii],X0)
+# end 
+t,tmpOUT = loop_fit(l,tmpOUT,model,times,ydata,X0)
+
+#= 
+This @btime macro is for testing the script time several times. We'll see if mulit threading is one-off or truely slow 
+=#
+@btime loop_fit(l,tmpOUT,model,times,ydata,X0)
+
+println("The actual fit took $t seconds")
+perS=Int(floor(l/t))
+println("$perS voxels per second")
+Xv = tmpOUT
+
+_,n = size(Yy)    
+fit_data = zeros(n,4);
+
+for ii ∈ 1:length(ind)
+    fit_data[ind[ii],:] = Xv[ii,:]
+end
+
+# fill in preallocated maps, reshape, clip, and save the maps.
+PSR = reshape(fit_data[:,1].*100,nx,ny,nz)
+R1f = reshape(fit_data[:,2],nx,ny,nz)
+Sf = reshape(fit_data[:,3],nx,ny,nz)
+
+PSR[findall(x->x.>100,PSR)].=0
+PSR[findall(x->x.<0,PSR)].=0
+R1f[findall(x->x.>50,R1f)].=0
+R1f[findall(x->x.<0,R1f)].=0
+
+Sf[findall(x->x.>10,Sf)].=0
+Sf[findall(x->x.<-10,Sf)].=0
+
+d=niread(paths);
+
+tmp1 = voxel_size(d.header)[1]
+tmp2 = voxel_size(d.header)[2]
+tmp3 = voxel_size(d.header)[3]
+
+PSR_fname = @sprintf("%s/SIR_Brain_PSR_julia.nii.gz",base) # I'm thinking that we could get rid of sprintf and use $base/<filename>
+R1f_fname = @sprintf("%s/SIR_Brain_R1f_julia.nii.gz",base)
+SF_fname = @sprintf("%s/SIR_Brain_Sf_julia.nii.gz",base)
+
+fname_1 = @sprintf("%s/SIR_DATA.nii.gz",base)
+niwrite(PSR_fname,NIVolume(PSR;voxel_size=(tmp1,tmp2,tmp3)))
+niwrite(R1f_fname,NIVolume(R1f;voxel_size=(tmp1,tmp2,tmp3)))
+niwrite(SF_fname,NIVolume(Sf;voxel_size=(tmp1,tmp2,tmp3)))
+
+niwrite(fname_1,NIVolume(temp2;voxel_size=(tmp1,tmp2,tmp3)))
+
+println(" ") # These println's are to add spacing in the command line output
 println(" ")
 println(" ")
-println(" ")
-println("The entire script took $tt seconds")
+println("The PSR is saved at $PSR_fname")
+println("The R1f is saved at $R1f_fname")
+println("The Sf is saved at $SF_fname")
+println("The original data was copied to $fname_1")
+println(" in the same image space as the PSR map")
+
+
+# println(" ")
+# println(" ")
+# println(" ")
+# println("The entire script took $tt seconds")
