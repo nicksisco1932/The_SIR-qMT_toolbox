ac08d42c96df99feb12bd2baff5e5d171f8afa1e
nicksisco1932 <nicksisco1932@gmail.com>
2022-01-27 11:03:42 -0700

    Update run_me.sh
    sed $'s/\r$//' ./run_me.sh  >temp.sh
    mv temp.sh run_me.sh

--- run_me.sh
+++ run_me.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
-
-path=/mnt/c/Users/nicks/Documents/MRI_data/PING_brains/TestRetest/output_20210618/proc_20210618/
+#!/usr/bin/env bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+path=$SCRIPT_DIR/Data_used_for_publication/SIR_brains/
+# path=/mnt/c/Users/nicks/Documents/MRI_data/PING_brains/TestRetest/output_20210618/proc_20210618/
 SIR_4D_DATA=$path/SIR_DATA.nii.gz
 brain_mask=$path/brain_mask.nii.gz
-julia ./SIR_fit.jl --TI 15 15 278 1007 --TD 684 4121 2730 10 --SIR_Data $SIR_4D_DATA --SIR_brainMask $brain_mask --kmf 12.5 --Sm 0.83
+julia --threads=auto ./SIR_fit.jl --TI 15 15 278 1007 --TD 684 4121 2730 10 --SIR_Data $SIR_4D_DATA --SIR_brainMask $brain_mask --kmf 12.5 --Sm 0.83
